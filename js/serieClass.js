9b1aba64eff264b74cc1f999388b37071030e307
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-07-07 03:18:25 -0300

    agrego clase series

---
+++ js/serieClass.js
@@ -0,0 +1,10 @@
+export class Serie {
+  constructor(codigo, titulo, descripcion, imagen, genero) {
+    this.codigo = codigo;
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.genero = genero;
+  }
+  // Agregar los metodos necesarios
+}
