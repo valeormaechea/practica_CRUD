782052b84ba7f5c6535f2241d920445e98c5c9cf
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-07-07 09:38:26 -0300

    agrego getters y setters y funcion para truncar

--- js/serieClass.js
+++ js/serieClass.js
@@ -6,5 +6,44 @@ export class Serie {
     this.imagen = imagen;
     this.genero = genero;
   }
-  // Agregar los metodos necesarios
+
+  get get_codigo() {
+    return this.codigo;
+  }
+
+  get get_titulo() {
+    return this.titulo;
+  }
+
+  get get_descripcion() {
+    return this.descripcion;
+  }
+
+  get get_imagen() {
+    return this.imagen;
+  }
+
+  get get_genero() {
+    return this.genero;
+  }
+
+  set set_codigo(codigoNuevo) {
+    this.codigo = codigoNuevo;
+  }
+
+  set set_titulo(tituloNuevo) {
+    this.titulo = tituloNuevo;
+  }
+
+  set set_descripcion(descripNueva) {
+    this.descripcion = descripNueva;
+  }
+
+  set set_imagen(imagenNueva) {
+    this.imagen = imagenNueva;
+  }
+
+  set set_genero(generoNuevo) {
+    this.genero = generoNuevo;
+  }
 }
